8ef212af8c8f358c147e31b64b259be0d90fd816
Groodev <104547527+Groodev@users.noreply.github.com>
2024-12-20 11:25:01 +0700

    Update Zpiecetest.lua

--- Zpiecetest.lua
+++ Zpiecetest.lua
@@ -1,1 +1,13 @@
-
+15060257865
+function CheckQuest() 
+    MyLevel = game:GetService("Players").LocalPlayer.Data.Level.Value
+    if World1 then
+        if MyLevel == 1 or MyLevel <= 12000 then
+            Mon = "Bandit"
+            LevelQuest = 1
+            NameQuest = "BanditQuest1"
+            NameMon = "Bandit"
+            CFrameQuest = CFrame.new(7767.24561, 128.480759, -5224.30127, -0.388562113, 2.20055352e-08, 0.921422541, 3.27933725e-09, 1, -2.24992434e-08, -0.921422541, -5.72069814e-09, -0.388562113)
+            CFrameMon = CFrame.new(7818.45654, 128.481079, -5292.70557, -0.0416653827, -4.65676742e-08, -0.99913162, -1.02101147e-07, 1, -4.23503685e-08, 0.99913162, 1.00247938e-07, -0.0416653827)
+        elseif World2 then 
+      if MyLevel == 
